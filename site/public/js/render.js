a6ad44e265c7314165e66416ee4bcefdfaee80a2
Brandon <Brandon@Brandon.com>
2017-11-01 07:53:09 -0400

    updates params.html and render.js

--- site/public/js/render.js
+++ site/public/js/render.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', function() {
-        
-    document.getElementById("states").addEventListener("change", function() { getCounties() });
-    document.getElementById("states").addEventListener("change", function() { getCountyResultsByState() });
-    document.getElementById("elections").addEventListener("change", function() { getCountyResultsByState() });
-    getElections();
-    getStates();
+    
+    $('#params').load('/snippets/params.html', function() {
+        document.getElementById("states").addEventListener("change", function() { getCounties() });
+        document.getElementById("states").addEventListener("change", function() { getCountyResultsByState() });
+        document.getElementById("elections").addEventListener("change", function() { getCountyResultsByState() });
+        getElections();
+        getStates();
+    })
     // getCountyResultsByState()        
 });
