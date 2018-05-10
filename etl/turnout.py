a0ff036136e25130dca509cd8bf1cd70fd503c1d
Live session user <BrandonSmith114@gmail.com>
2018-05-10 08:00:36 -0400

    adds new etl files for turnout calculation

--- etl/turnout.py
+++ etl/turnout.py
@@ -4,26 +4,39 @@ import pandas as pd
 import vote
 
 '''
-cleans up raw cvap file from census for 2016
+cleans up raw cvap file from census for elections
 '''
 
 raw_data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','raw-data'))
 clean_data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..','data'))
 
-# reads cvs into dataframe
-cvap = pd.read_csv(os.path.join(raw_data_path,'cvap-2016.csv'))
+dfs = []
 
-# creates new CVAP columns fips and election
-cvap['fips'] = cvap['GEOID'].str[-5:].astype(int)
-cvap['election'] = 2016 
+for election in [2012, 2016]:
+    # cvap file path
+    cvap_file = os.path.join(raw_data_path, 'cvap-' + str(election) + '.csv')
 
-# filtes cvap to total voters
-cvap = cvap.query('LNNUMBER == 1')
-cvap = cvap[['fips','election','CVAP_EST']]
+    # reads cvs into dataframe
+    cvap = pd.read_csv(cvap_file)
+
+    # creates new CVAP columns fips and election
+    cvap['fips'] = cvap['GEOID'].str[-5:].astype(int)
+    cvap['election'] = election 
+
+    # filtes cvap to total voters
+    cvap = cvap.query('LNNUMBER == 1')
+    cvap = cvap[['fips','election','CVAP_EST']]
+    cvap.rename(columns={'CVAP_EST': 'cvap'}, inplace=True)
+
+    # appends dataframe to list
+    dfs.append(cvap)
+
+# concats dataframes for all years
+cvap = pd.concat(dfs)
 
 # resets index
 cvap.reset_index(inplace=True)
-cvap.rename(columns={'CVAP_EST': 'cvap'}, inplace=True)
-cvap.drop(columns=['index'], inplace=True)
 
-cvap.to_csv(os.path.join(clean_data_path, 'cvap.csv'))
+# adds turnout change for 2016
+
+# creates dataframe turnout dataframe
